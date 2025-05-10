d7bd2ad10a0cfb4ccc6d960c0a3dcafe57358ffe
dustytrail33 <dustytrail33@mail.ru>
2025-05-10 19:39:30 +0300

    feat: added deleteUser route

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import notFound from './routes/notFound';
 import getUser from './routes/getUser';
 import createUser from './routes/createUser';
 import changeUser from './routes/changeUser';
+import deleteUser from './routes/deleteUser';
 
 const router = async (req: IncomingMessage, res: ServerResponse) => {
   const url = req.url ? parse(req.url, true) : parse('', true);
@@ -24,11 +25,15 @@ const router = async (req: IncomingMessage, res: ServerResponse) => {
     return getUser(res, userId);
   }
 
-  if(matchId && method === "PUT") {
-    const userId = matchId[1]
-    return await changeUser(req, res, userId)
+  if (matchId && method === 'PUT') {
+    const userId = matchId[1];
+    return await changeUser(req, res, userId);
   }
 
+  if (matchId && method === 'DELETE') {
+    const userId = matchId[1];
+    return await deleteUser(res, userId);
+  }
 
   return notFound(res);
 };
--- src/utils/sendData.ts
+++ src/utils/sendData.ts
@@ -3,6 +3,12 @@ import { ServerResponse } from 'http';
 const sendData = (data: unknown, res: ServerResponse, status: number) => {
   const jsonData = JSON.stringify(data);
 
+  if (data === null) {
+    res.writeHead(status);
+    res.end();
+    return;
+  }
+
   res.writeHead(status, {
     'Content-Type': 'application/json',
   });
